2723d81c21b2714bbefd719b0a05abb917486e62
Sudarshanbalichakra <suddu680@gmail.com>
2022-09-09 12:31:30 +0530

    added banana talk

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const textInput=document.querySelector("#txt-input")
 const btnTranslate=document.querySelector("#btn-translate")
 const output=document.querySelector("#output")
 
-var URL="https://api.funtranslations.com/translate/minion.json";
+var URL="https://api.funtranslations.com/translate/minion.json"
 
 function getTransaltionURL(text){
     return URL+ "?"+ "text=" + text 
 }
 
+function erorrhandler(erorr){
+    console.log("erorr occured",erorr);
+    alert("something went wrong please try again after some time ")
+}
+
 function clickHandler(){
-    var inputText=textInput.value;
+    var  inputText=textInput.value;
 
     fetch(getTransaltionURL(inputText))
         .then(response=>response.json())
@@ -17,6 +22,7 @@ function clickHandler(){
             let translatedText=json.contents.translated;
             output.innerText=translatedText;
         })
+        .catch(erorrhandler)
 };
 
 btnTranslate.addEventListener("click",clickHandler);
